bac4b5295b57eca82441dd2a3a3167a501ff6f71
alexgamez <alex_gamez93@hotmail.com>
2019-07-15 16:14:43 -0500

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+country = Country.create(name: "ESPAÑA")
+country.states.create(name: "MADRID")
+
+country = Country.create(name: "ITALIA")
+country.states.create(name: "ROMA")
+
+country = Country.create(name: "JAPON")
+country.states.create(name: "TOKIO")
+
+country = Country.create(name: "COSTA RICA")
+country.states.create(name: "MADRID")
+
+country = Country.create(name: "ESPAÑA")
+country.states.create(name: "SAN JOSE")
